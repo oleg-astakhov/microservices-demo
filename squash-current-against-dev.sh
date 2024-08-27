bc958428661ce21e4dd252fe9ff6e6e61a3c164d
Oleg Astakhov <java.oleg@gmail.com>
2024-08-27 18:37:19 +0300

    update readme and squash git script

--- squash-current-against-dev.sh
+++ squash-current-against-dev.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 
-debug_trap () {
-    echo "executing $BASH_COMMAND"
-    echo "Allow?"
+debug_trap() {
+    echo "Executing command that will squash all commits on current branch that don't exist on dev branch: $BASH_COMMAND"
+    echo "Do you want to continue? (1 = yes / 2 = no)"
     select choice in yes no
     do
-        if [ "$choice" = "yes" ]; then
-          eval $BASH_COMMAND
-          exit 0
-        elif [ "$choice" = "no" ]; then
-          exit 1
-        fi
+        case $choice in
+            yes)
+                eval "$BASH_COMMAND"
+                exit 0
+                ;;
+            no)
+                exit 1
+                ;;
+            *)
+                echo "Invalid choice. Please type in '1' (yes) or '2' (no)"
+                ;;
+        esac
     done
 }
 
+# Enable extdebug option to use DEBUG trap
 shopt -s extdebug
 trap debug_trap DEBUG
-git reset --soft `git merge-base HEAD dev`
+
+git reset --soft "$(git merge-base HEAD dev)"
